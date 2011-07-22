7930db377bf9b6becf1990e7bbba599e511e7237
dawagner <dawagner@b94323b0-7eef-fcb5-5db8-2ccef1d9dae9>
2011-07-22 22:15:06 +0000

    Adding basic tests

---
+++ test/tests/common.js
@@ -0,0 +1,40 @@
+var plugin = document.createElement("embed");
+plugin.setAttribute("type", "application/x-npapi-file-io");
+document.documentElement.appendChild(plugin);
+
+function getPlatform() {
+	return "windows";
+}
+
+function getPath(filename) {
+	if (getPlatform() === "windows") {
+		return "C:\\svn\\npapi-file-io\\test\\files\\" + filename;
+	}
+	throw "Unsupported platform";
+}
+
+function getRandom() {
+  return Math.floor(Math.random() * 100000000000000);
+}
+
+function assertThrows(fun) {
+  try {
+    fun();
+    fail("Expected exception");
+  } catch (e) {
+    if (e.isJsUnitFailure !== undefined) {
+      throw e;
+    }
+  }
+}
+
+function callWithNonStringThrows(fun) {
+  assertThrows(function () { fun(1); });
+  assertThrows(function () { fun(true); });
+  assertThrows(function () { fun({}); });
+  assertThrows(function () { fun([]); });
+}
+
+function callWithTwoArgumentsThrows(fun) {
+  assertThrows(function() { fun("foo", "bar"); });
+}
