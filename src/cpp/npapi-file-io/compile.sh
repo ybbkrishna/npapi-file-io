70de47fdcaa1bf7d2398305ec4da7b73a26d1d9d
dawagner <dawagner@b94323b0-7eef-fcb5-5db8-2ccef1d9dae9>
2011-07-30 23:44:34 +0000

    Turn on optimisations

--- src/cpp/npapi-file-io/compile.sh
+++ src/cpp/npapi-file-io/compile.sh
@@ -1,4 +1,4 @@
 for BITS in 32 64
 do
-  g++ npapi-file-io.cpp file-io.cpp -c -fPIC -Wall -DOS_LINUX=1 -I"third_party" -I"third_party/npapi" -m$BITS && g++ -shared -W1,-soname,npapi-file-io-$BITS.so -o npapi-file-io-$BITS.so npapi-file-io.o file-io.o -m$BITS && cp npapi-file-io-$BITS.so ../../../test/extension/
+  g++ npapi-file-io.cpp file-io.cpp -c -fPIC -Wall -DOS_LINUX=1 -I"third_party" -I"third_party/npapi" -m$BITS -O3 && g++ -shared -W1,-soname,npapi-file-io-$BITS.so -o npapi-file-io-$BITS.so npapi-file-io.o file-io.o -m$BITS -O3 && cp npapi-file-io-$BITS.so ../../../test/extension/
 done
