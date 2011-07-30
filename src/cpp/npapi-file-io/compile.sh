a37ed15c80b21b95ccff44613d317675b2a85871
dawagner <dawagner@b94323b0-7eef-fcb5-5db8-2ccef1d9dae9>
2011-07-30 17:44:05 +0000

    Compile 32 and 64 bit versions

--- src/cpp/npapi-file-io/compile.sh
+++ src/cpp/npapi-file-io/compile.sh
@@ -1,1 +1,4 @@
-g++ npapi-file-io.cpp file-io.cpp -c -fPIC -Wall -DOS_LINUX=1 -I"third_party" -I"third_party/npapi" -m32 && g++ -shared -W1,-soname,npapi-file-io-32.so -o npapi-file-io-32.so npapi-file-io.o file-io.o -m32 && cp npapi-file-io-32.so ../../../test/extension/
+for BITS in 32 64
+do
+  g++ npapi-file-io.cpp file-io.cpp -c -fPIC -Wall -DOS_LINUX=1 -I"third_party" -I"third_party/npapi" -m$BITS && g++ -shared -W1,-soname,npapi-file-io-$BITS.so -o npapi-file-io-$BITS.so npapi-file-io.o file-io.o -m$BITS && cp npapi-file-io-$BITS.so ../../../test/extension/
+done
